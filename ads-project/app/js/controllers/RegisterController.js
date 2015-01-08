a1e6bee66b12dd44d12fdd743fa61743d7c5019b
Teodora Vacheva <ozvacheva@gmail.com>
2015-01-08 14:49:47 +0200

    Skeleton changed

---
+++ ads-project/app/js/controllers/RegisterController.js
@@ -0,0 +1,13 @@
+app.controller('RegisterController', ['$scope', 'townsData', 'userData',
+    function($scope, townsData, userData){
+
+        townsData.getTowns()
+            .$promise
+            .then(function (data){
+               $scope.towns = data;
+        });
+
+        $scope.register = function(user){
+            userData.register(user);
+    };
+}]);
